6b3462141ac68785a8a035f1f2e86321085e1d41
Jon Dawson <dawson.jon@hotmail.com>
2021-07-07 17:45:39 +0100

    constructing PDF

--- components/pdfCreator.js
+++ components/pdfCreator.js
@@ -2,21 +2,44 @@ import Head from 'next/head'
 import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
 import { Document, Page, Text, View, StyleSheet, PDFDownloadLink, PDFViewer } from '@react-pdf/renderer';
+import { colors, sizes, font } from '../styles/variables'
 
 // Create styles
 const styles = StyleSheet.create({
   page: {
     justifyContent: 'center',
     flexDirection: 'column',
-    backgroundColor: '#FFFFFF',
+    backgroundColor: `${colors.white}`,
     alignItems: 'center'
   },
   section: {
+    alignItems: 'center',
     margin: 10,
+    marginTop: 40,
     padding: 10,
-    flexGrow: 1
+    flex: 1,
+  },
+  body1: {
+    fontSize: 14,
+    marginBottom: 30,
+    marginTop: 10,
+  },
+  body2: {
+    fontSize: 32,
+    marginBottom: 30,
+    color: '#6236FF',
+  },
+  body3: {
+    fontSize: 12,
+    marginBottom: 30,
+    // marginLeft: 140,
+    // marginRight: 140,
+    // textAlign: 'center'
   }
 });
+
+const name = 'Tamara-Drew'
+const athlete = 'Battlestar CP3-0 Hockey Player'
 
 // Create Document Component
 const PDFDocument = () => {
@@ -24,11 +47,16 @@ const PDFDocument = () => {
   <Document>
     <Page size="A4" style={styles.page}>
       <View style={styles.section}>
-        <Text>Section #1</Text>
-      </View>
-      <View style={styles.section}>
-        <Text>Section #2</Text>
-      </View>
+        <Text>.......................</Text>
+        <Text style={styles.body1}>THIS IS TO CERTIFY THAT</Text>
+        <Text style={styles.body2}>{name}</Text>
+        <Text style={styles.body3}>Has successfully created an Interglatic Athlete in the pursuit of challenging planet X-R23's Dream Team.</Text>
+        <Text>.......................</Text>
+        <Text style={styles.body1}>CALLED</Text>
+        <Text style={styles.body2}>{athlete}</Text>
+        <Text style={styles.body3}>Your effort will now help Earth complete in the ultimate match.</Text>
+        <Text style={styles.body3}>Sincerely, thank you.</Text>
+      </View>   
     </Page>
   </Document>
   )
---
+++ styles/variables.js
@@ -0,0 +1,77 @@
+// import './font.css'
+
+const sizes = {
+  xs: 12,
+  sm: 14,
+  base: 16,
+  md: 24,
+  lg: 36,
+  xl: 42,
+  xxl: 48,
+  xxxl: 64,
+}
+
+const breakpoints = {
+  largeDesktop: 1200,
+  desktop: 992,
+  tablet: 767,
+  phablet: 480,
+  phone: 320,
+}
+
+const colors = {
+  white: '#FFFFFF',
+  black: '#000000',
+  grey: '#CCCCCC',
+  smoke: '#F5F5F5',
+
+  // ocean color palette
+  coral: '#FF6F61',
+  pink: '#FDC4B6',
+  sand: '#E59572',
+  teal: '#2694AB',
+  turquoise: '#4DBEDF',
+
+  transparent: 'rgba(255,255,255,0)',
+  overlay: 'rgba(00,00,00,0.5)',
+}
+
+const easing = {
+  hover: 'all 0.4s cubic-bezier(0.215, 0.61, 0.355, 1) 0s',
+  default: '0.6s ease-in 0s',
+  bezier: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+}
+
+const transitions = {
+  offset: '100',
+  delay: '300',
+}
+
+const coverImage = {
+  backgroundPosition: 'center center',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+}
+
+const font = {
+  family: {
+    body: 'DM Sans, Arial, sans-serif',
+    heading: 'DM Sans, Arial, sans-serif',
+    mono: 'Inconsolata, monospace',
+    custom: 'Dollie',
+    custom2: 'BrilliantCut',
+    custom3: 'ObjectSans-Heavy',
+    custom4: 'GoshaSans-Bold'
+  },
+  weight: {
+    regular: 400,
+    medium: 500,
+    bold: 700,
+  },
+  lineheights: {
+    body: 1.5,
+    heading: 1.1,
+  },
+}
+
+export { breakpoints, colors, coverImage, easing, font, sizes, transitions }
