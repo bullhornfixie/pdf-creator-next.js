96d6d933b64bdb57edadb1a160761550e896b6ae
Jon Dawson <dawson.jon@hotmail.com>
2021-07-08 11:55:11 +0100

    url image rendering only

--- components/pdfCreator.js
+++ components/pdfCreator.js
@@ -1,8 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
 // import styles from '../styles/Home.module.css'
-import { Document, Page, Text, View, StyleSheet, PDFDownloadLink, PDFViewer } from '@react-pdf/renderer';
+import { Document, Page, Text, View, StyleSheet, Image } from '@react-pdf/renderer';
+import { PDFDownloadLink, PDFViewer} from '@react-pdf/renderer';
 import { colors, sizes, font } from '../styles/variables'
+import Logo from './vercel.svg'
 
 // Create styles
 const styles = StyleSheet.create({
@@ -35,6 +36,11 @@ const styles = StyleSheet.create({
     // marginLeft: 140,
     // marginRight: 140,
     // textAlign: 'center'
+  },
+  image: {
+    width: 400,
+    height: 180,
+    backgroundColor: `${colors.black}`
   }
 });
 
@@ -42,30 +48,31 @@ const name = 'Tamara-Drew'
 const athlete = 'Battlestar CP3-0 Hockey Player'
 
 // Create Document Component
-const PDFDocument = () => {
+const PDFDocument = ({image}) => {
   return (
-  <Document>
-    <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
-        <Text>.......................</Text>
-        <Text style={styles.body1}>THIS IS TO CERTIFY THAT</Text>
-        <Text style={styles.body2}>{name}</Text>
-        <Text style={styles.body3}>Has successfully created an Interglatic Athlete in the pursuit of challenging planet X-R23's Dream Team.</Text>
-        <Text>.......................</Text>
-        <Text style={styles.body1}>CALLED</Text>
-        <Text style={styles.body2}>{athlete}</Text>
-        <Text style={styles.body3}>Your effort will now help Earth complete in the ultimate match.</Text>
-        <Text style={styles.body3}>Sincerely, thank you.</Text>
-      </View>   
-    </Page>
-  </Document>
+    <Document> 
+      <Page size="A4" style={styles.page} >
+        <View style={styles.section} >
+          <Text style={styles.body1}>THIS IS TO CERTIFY THAT</Text>
+          <Text style={styles.body2}>{name}</Text>
+          <Text style={styles.body3}>Has successfully created an Interglatic Athlete in the pursuit of challenging planet X-R23's Dream Team.</Text>
+          <Text>.......................</Text>
+          <Text style={styles.body1}>CALLED</Text>
+          <Text style={styles.body2}>{athlete}</Text>
+          <Text style={styles.body3}>Your effort will now help Earth complete in the ultimate match.</Text>
+          <Text style={styles.body3}>Sincerely, thank you.</Text>
+          <Image style={styles.image} src="http://static.donquijote.org/images/blogs/dq-reg/don-quijote-de-la-mancha.jpg" alt="image" />
+          <Image src={image} style={styles.image} />
+        </View>   
+      </Page>
+    </Document>
   )
 }
 
 const Output = () => (
   <>
   <PDFViewer height='900vh' width='1200vh'>
-     <PDFDocument />
+     <PDFDocument image="http://static.donquijote.org/images/blogs/dq-reg/don-quijote-de-la-mancha.jpg" />
   </PDFViewer>
 </>
 )
