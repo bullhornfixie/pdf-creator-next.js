1a63387e928424f94bc43e2676eeeb65a6a90a15
Jon Dawson <dawson.jon@hotmail.com>
2021-07-08 12:58:14 +0100

    border inserted

--- components/pdfCreator.js
+++ components/pdfCreator.js
@@ -3,7 +3,9 @@ import Head from 'next/head'
 import { Document, Page, Text, View, StyleSheet, Image } from '@react-pdf/renderer';
 import { PDFDownloadLink, PDFViewer} from '@react-pdf/renderer';
 import { colors, sizes, font } from '../styles/variables'
-import Logo from './loooogo.jpg'
+import Logo from '../public/certify.png'
+
+console.log({Logo})
 
 // Create styles
 const styles = StyleSheet.create({
@@ -14,11 +16,15 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   section: {
-    alignItems: 'center',
     margin: 10,
-    marginTop: 40,
-    padding: 10,
-    flex: 1,
+    paddingTop: 55,
+    paddingBottom: 55,
+    paddingHorizontal: 70,
+    float: 'left',
+    textAlign: 'center',
+    borderRadius: 0,
+    borderWidth: 1.5,
+    borderColor: '#D7D8D6',
   },
   body1: {
     fontSize: 14,
@@ -26,7 +32,7 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   body2: {
-    fontSize: 32,
+    fontSize: 30,
     marginBottom: 30,
     color: '#6236FF',
   },
@@ -38,9 +44,13 @@ const styles = StyleSheet.create({
     // textAlign: 'center'
   },
   image: {
-    width: 400,
-    height: 180,
-    backgroundColor: `${colors.black}`
+    marginBottom: 25,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    height: 120,
+    width: 120,
+    display: 'block',
+    backgroundColor: `${colors.black}`,
   }
 });
 
@@ -51,7 +61,7 @@ const athlete = 'Battlestar CP3-0 Hockey Player'
 const PDFDocument = ({image}) => {
   return (
     <Document> 
-      <Page size="A4" style={styles.page} >
+      <Page size="A4" style={styles.page}>
         <View style={styles.section} >
           <Text style={styles.body1}>THIS IS TO CERTIFY THAT</Text>
           <Text style={styles.body2}>{name}</Text>
@@ -61,7 +71,7 @@ const PDFDocument = ({image}) => {
           <Text style={styles.body2}>{athlete}</Text>
           <Text style={styles.body3}>Your effort will now help Earth complete in the ultimate match.</Text>
           <Text style={styles.body3}>Sincerely, thank you.</Text>
-          <Image style={styles.image} src="http://static.donquijote.org/images/blogs/dq-reg/don-quijote-de-la-mancha.jpg" alt="image" />
+          <Image style={styles.image} cache={false} src={Logo}></Image>
         </View>   
       </Page>
     </Document>
@@ -69,11 +79,9 @@ const PDFDocument = ({image}) => {
 }
 
 const Output = () => (
-  <>
   <PDFViewer height='900vh' width='1200vh'>
      <PDFDocument image="http://static.donquijote.org/images/blogs/dq-reg/don-quijote-de-la-mancha.jpg" />
   </PDFViewer>
-</>
 )
 
 export default Output
